1c702891d23064180df9793207021ce42d824b9e
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-10-29 21:25:12 -0500

    wip: diplay shows

--- src/pages/ShowsIndex.js
+++ src/pages/ShowsIndex.js
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 const ShowsIndex =(props) => {
-    return <h1>ShowsIndex</h1>
+    const loaded = () => {
+        return props.show.map(show => (
+            <div key={show._id} className="show">
+                <Link to={`/shows/${show._id}`}>
+                    <h1>{show.date}</h1>
+                </Link>
+                <h3>{show.venue}</h3>
+                <h3>{show.info}</h3>
+            </div>
+        ))
+    }
+    const loading = () => <h1>Loading ...</h1>
+
+    return props.show ? loaded() : loading()
 };
 
 

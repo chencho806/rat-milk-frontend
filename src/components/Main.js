5a3f59efc19600ca3deac7182527b3a30d4cb994
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-10-29 21:08:59 -0500

    wip: intializes state and adds helper functions

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,27 @@ import Show from '../pages/Show';
 
 
 const Main = (props) => {
+    const [ show, setShow ] = useState(null);
+
+    const BASE_URL = 'http://localhost:3001/shows/';
+
+    const getShow = async () => {
+        const data = await fetch(BASE_URL).then(response => response.json());
+        setShow(data);
+    }
+
+    const createShow = async (show) => {
+        await fetch(BASE_URL, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'Application/json'
+            },
+            body: JSON.stringify(show)
+        });
+
+        getShow();
+    }
+
     return (
         <main>
             <Switch>

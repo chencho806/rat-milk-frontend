cf18ea408d063c8dd03590bf0c1cf176b7ae33b1
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-11-04 23:56:44 -0500

    wip: adds image to listen page

--- src/pages/Listen.js
+++ src/pages/Listen.js
@@ -9,12 +9,17 @@ const Listen=(props) => {
   const theme = 'black'; 
   
   return (
+    <>
+    <div>
+      <img src="https://i.imgur.com/ruWZI7e.jpg?1" alt="" className="listenImage"/>
+    </div>
     <SpotifyPlayer
       uri= "https://open.spotify.com/album/3UrUNyumXYfBUynAvcyMC9"
       size={size}
       view={view}
       theme={theme}
     />
+    </>
   )
 };
 
